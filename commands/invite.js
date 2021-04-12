6b1fd0b6bd5301a874ac9a8835f47cd89e49dc08
get543 <66203118+get543@users.noreply.github.com>
2021-04-12 20:17:21 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,17 +1,15 @@
-const Discord = require('discord.js');
-
 module.exports = {
     name: 'invite',
     description: "send an invite link",
     execute(message, args, Discord) {
-        message.channel.send('https://discord.com/oauth2/authorize?client_id=811562521957498910&scope=bot&permissions=0')
+        message.channel.send('https://discord.com/oauth2/authorize?client_id=811562521957498910&scope=bot&permissions=0');
         
-        const invite = new Discord.MessageEmbed()
-        .setTitle('AndroidBot | Bot Bisa Apa Aja')
-        .setDescription('ini bot bisa ngapain aja terserah lu dah')
-        .setAuthor('udin')
-        .setColor('#7FFFD4')
+        // const invite = new Discord.MessageEmbed()
+        // .setTitle('AndroidBot | Bot Bisa Apa Aja')
+        // .setDescription('ini bot bisa ngapain aja terserah lu dah')
+        // .setAuthor('udin')
+        // .setColor('#7FFFD4')
 
-        message.channel.send(invite);
+        // message.channel.send(invite);
     }
 }
