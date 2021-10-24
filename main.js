0ed7353ecb9181697f3509061a203b24d8ee2286
ghazapratama <fghaza.pratama@gmail.com>
2021-10-24 21:10:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send(`<h1>Hello World!</h1> \n <p>This is a webserver for the discord bot</p>`));
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+// !Bot Code
 const Discord = require('discord.js');
 require('dotenv').config();
 const { resolveSoa } = require('dns');
