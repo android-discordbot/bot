b0d58e49da2cf57b2fbb9b570e41371d3de8e2bd
ghazapratama <fghaza.pratama@gmail.com>
2021-11-08 16:51:42 +0700

    update

--- web/app.js
+++ web/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send(`<h1>Hello World!</h1> \n <p>This is a webserver for the discord bot</p>`));
+// register view engine
+app.set('view engine', 'ejs');
+app.set('views', 'web');
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
