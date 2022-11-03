f8f49ea658a881f895fc45c1ec9a3ba54a2a5d2e
Udin <fghaza.pratama@gmail.com>
2022-11-03 16:49:42 +0700

    nowplaying command works

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -12,16 +12,8 @@ module.exports = {
         if(!song) return message.channel.send('No Music Is Being Played.');
         
         try {
-            // const duration = song.formattedDuration.split(':').reverse().reduce((prev, curr, i) => prev + curr * Math.pow(60, i), 0);
-
-            // const seek = Math.floor((server_queue.connection.dispatcher.streamTime - server_queue.connection.dispatcher.pausedTime) / 1000) + 1;
-
-            // const actualSeek = new Date(seek * 1000).toISOString().substr(11, 8);
-
-            // const timeLeft = new Date((duration - actualSeek) * 1000).toISOString().substr(11, 8);
-            
-            const actualSeek = Math.floor(song.formattedCurrentTime / 1000);
-            let finalTotal = song.formattedDuration;
+            const actualSeek = server_queue.formattedCurrentTime;
+            const finalTotal = song.formattedDuration;
     
             const npEmbed = new Discord.MessageEmbed()
                 .setColor('#7FFF00')
@@ -30,13 +22,12 @@ module.exports = {
                 .setURL(song.url)
                 .setThumbnail(song.thumbnail)
                 .setDescription(`${actualSeek} / ${finalTotal}`)
-                // .addField('Time Left:', timeLeft)
                 .setTimestamp()
                 .setFooter(`Requested by: ${message.author.username}`)
             message.channel.send(npEmbed);
         } catch (error) {
-            console.log(error);
-            message.channel.send(`Still trying to fix it 😖`);
+            console.error(error);
+            message.channel.send('Still trying to fix it 😖');
         };
     },
 };
