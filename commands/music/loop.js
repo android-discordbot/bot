bd7ffb21eac1053dc8533a00e81604340c2f8b6e
get543 <fghaza.pratama@gmail.com>
2024-08-16 21:36:39 +0700

    change strings & using prefix for example

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -3,12 +3,14 @@ module.exports = {
   aliases: ["repeat"],
   description: "put the song on loop",
   async execute(client, message, args, cmd, Discord) {
+    const prefix = process.env.PREFIX;
+
     if (!message.member.voice.channel)
       return message.channel.send("You need to be in a voice channel first. 🤪");
 
     if (!args[0]) {
       message.channel.send(
-        "loop 1 for repeating a song \nloop 2 for repeating all the queue \nloop 0 for disabled"
+        `**${prefix}loop 1** for repeating a song \n**${prefix}loop 2** for repeating all the queue \n**${prefix}loop 0** for disabled`
       );
     } else if (args[0] == 1) {
       client.distube.setRepeatMode(message, 1);
@@ -20,7 +22,7 @@ module.exports = {
       client.distube.setRepeatMode(message, 0);
       message.channel.send("Repeat mode is set to disabled.");
     } else {
-      return message.channel.send("You either put 1 (song) or 2 (queue) or 0 (disabled), nothing else!");
+      return message.channel.send("You either put 1 (loop song) or 2 (loop queue) or 0 (disabled), nothing else!");
     }
   },
 };
